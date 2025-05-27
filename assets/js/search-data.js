b6cb016d6a1ce8b76a473751c591c94c57f3f1e9
freedomFu <40269368+freedomFu@users.noreply.github.com>
2025-05-27 00:17:04 +0000

    Deploying to gh-pages from @ FuYanduo/fuyanduo.github.io@d761015d9a10b2f1eb491d23abf5bf09cbe53a22 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -49,17 +49,6 @@ ninja.data = [{
             window.location.href = "/blog/2025/%E8%AF%BB%E5%90%8E%E6%84%9F%E4%B9%8B%E5%8E%9F%E5%AD%90%E4%B9%A0%E6%83%AF/";
           
         },
-      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
       },{id: "post-增肌-减脂和营养素",
         
           title: "增肌、减脂和营养素",
@@ -102,17 +91,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2023/%E7%A1%95%E5%A3%AB%E6%AF%95%E4%B8%9A%E5%90%8E%E7%9A%84%E4%B8%80%E5%B9%B4-%E5%A6%82%E6%9E%9C%E9%87%8D%E5%9B%9E%E9%82%A3%E4%B8%AA%E5%8D%81%E5%AD%97%E8%B7%AF%E5%8F%A3/";
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
       },{id: "post-专心专注",
